6ceba0d9598d9946309c44aeed98e941cc91ab85
Luckesh <37701339+Luckesh@users.noreply.github.com>
2019-01-31 23:08:36 +0100

    Updates

---
+++ precache-manifest.77f35ab46a65ab18dbfae89704e83aa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "373f966e0056b64302e879ad6c21f9ff",
+    "url": "/bellyreact/static/media/zlybg.373f966e.PNG"
+  },
+  {
+    "revision": "5a6eb8f88b223bb3b906991fb0d745fb",
+    "url": "/bellyreact/static/media/settlers.5a6eb8f8.jpg"
+  },
+  {
+    "revision": "ff80a4219fd73e49e18c15d8c08883f2",
+    "url": "/bellyreact/static/media/mena.ff80a421.jpg"
+  },
+  {
+    "revision": "201e5a846daa4cf92933",
+    "url": "/bellyreact/static/js/runtime~main.201e5a84.js"
+  },
+  {
+    "revision": "1412a959b4f37ac24fbb",
+    "url": "/bellyreact/static/js/main.1412a959.chunk.js"
+  },
+  {
+    "revision": "fd0b0740c970ac9799b1",
+    "url": "/bellyreact/static/js/1.fd0b0740.chunk.js"
+  },
+  {
+    "revision": "1412a959b4f37ac24fbb",
+    "url": "/bellyreact/static/css/main.077df093.chunk.css"
+  },
+  {
+    "revision": "b8713098d7a8e551c6a1688ff1229a3a",
+    "url": "/bellyreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.5601be9bf9453b5b6cf772029745f636.js"
+  "/bellyreact/precache-manifest.77f35ab46a65ab18dbfae89704e83aa1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/bellyreact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
