addad361c9be9066abafb6396fcb9d3b3fd546e0
Luckesh <37701339+Luckesh@users.noreply.github.com>
2019-03-24 20:13:13 +0100

    Update SearchGenre.js

--- src/components/SearchGenre.js
+++ src/components/SearchGenre.js
@@ -12,12 +12,29 @@ export class SearchGenre extends Component {
         this.state = {
           showModal: false,
           chosenGenres: {
-            "rodinné":0, 
-            "párty":0, 
+            "blafovacie":0,
+            "kartové":0,
+            "budovatelské":0,
+            "fantasy":0,
+            "stredoveké":0,
+            "politické":0,
             "vojnové":0,
-            "eurohry":0,
+            "abstraktné":0,
+            "kockové":0,
+            "sci-fi":0,
+            "social deduction":0,
+            "párty":0,
+            "staroveké":0,
+            "dobrodružné":0,
+            "bojové":0,
+            "strategické":0,
+            "tile placement":0,
+            "rodinné":0,
+            "základné":0,
+            "ekonomické":0,
             "postrehové":0,
             "kooperatívne":0,
+            "eurohry":0,
         },
         };
         
@@ -52,20 +69,25 @@ export class SearchGenre extends Component {
 
     render(){
         const { chosenGenres } = this.state;
+  
+   
         return (
             <div className='searchGenre'>
                 <div>
                      <h2>Aký žáner <span onClick={this.handleOpenModal} style={help}>?</span></h2>  
                        
                 </div>  
-                <div style={pole}>
-                    <Button onClick={() => this.changeStatus("rodinné")}toggle={chosenGenres["rodinné"]} text='Rodinné'/> 
-                    <Button onClick={() => this.changeStatus("párty")}toggle={chosenGenres["párty"]} text='Párty'/>
-                    <Button onClick={() => this.changeStatus("vojnové")}toggle={chosenGenres["vojnové"]} text='Vojnové'/>
-                    <Button onClick={() => this.changeStatus("eurohry")}toggle={chosenGenres["eurohry"]} text='Eurohry'/>
-                    <Button onClick={() => this.changeStatus("postrehové")}toggle={chosenGenres["postrehové"]} text='Postrehové'/>
-                    <Button onClick={() => this.changeStatus("kooperatívne")}toggle={chosenGenres["kooperatívne"]} text='Kooperatívne'/>
-                    
+                <div style={pole}>                  
+                    {
+                         Object.keys(chosenGenres).map(genre => {
+                            return(
+                                <Button onClick={() => this.changeStatus(genre)}toggle={chosenGenres[genre]} text={genre}/>
+                            )
+                           
+                         }
+                            
+                        )
+                    }
                     <div>
                         <p onClick={this.continue} style = {cta}>Vyhľadať</p>
                     </div>
